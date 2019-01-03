ae51e1aa21cf9ce512bf944757643807b8135621
Unknown <whdydql123@naver.com>
2019-01-03 14:53:51 +0900

    서브메뉴 탭으로 이동 시 가려지는 문제 수정
    서브 메뉴를 탭으로 이동 했을 경우에 아티클 콘텐츠에 의해 가려지는 문제 수정

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	 $(".menu-item").focusin(function(){
 		 var submenu = $(this).children("ul");
 		 if(submenu.length > 0){
+			 submenu.css("z-index", "99999");
 			 submenu.show();
 			 submenu.children("li:last-child").focusout(function(){
 				submenu.hide();
